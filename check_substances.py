2897d3e0550ba70cd66b83d497a61930aa952e79
El Pacho <75735030+franasal@users.noreply.github.com>
2025-05-06 18:42:14 +0200

    Update check_substances.py

--- check_substances.py
+++ check_substances.py
@@ -11,6 +11,9 @@ def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     data = {"chat_id": chat_id, "text": message}
     requests.post(url, data=data)
+    response = requests.post(url, data=data)
+    print("Telegram status:", response.status_code)
+    print("Response:", response.text)
 
 # Setup headless browser
 options = Options()
